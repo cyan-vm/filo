33442e210e53670c1bd1d711cec5a63107cfa9e3
cyan-vm <cyan.mv@gmail.com>
2024-04-22 08:40:20 -0600

    Subject crud completed

---
+++ database/migrations/2024_04_22_141701_create_subjects_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('subject', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('subject', 50);
+            $table->string('acronym', 45);
+            $table->integer('credits');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('subject');
+    }
+};
